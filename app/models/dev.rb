259ab036c9262b4bfd83e3a9266a7ace0112b571
Joe <zvmboni@gmail.com>
2022-05-10 17:57:16 -0700

    commit tree df1567fccd07068362b17adebbdb43b069fe397f

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,13 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        self.freebies.where(item_name: item_name).any?
+    end
+
+    def give_away(other_dev, freebie)
+        if freebie.dev_id == self.id
+            freebie.update(dev_id = other_dev.id)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder_mifflin = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+mr_meseeks = Dev.create(name: "Mr. Meseeks")
+gazorpazop = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,6 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: "Plushie", value: 10, company_id: google.id, dev_id: rick.id)
 
 puts "Seeding done!"
